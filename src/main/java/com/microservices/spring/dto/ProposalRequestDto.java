5c8e1982de689b6bf0aac2160d5499863b6732c2
daniel2dfla <daniel_2dfla@hotmail.com>
2024-09-18 08:24:03 -0300

    Feature: created the DTOs

--- src/main/java/com/microservices/spring/dto/ProposalRequestDto.java
+++ src/main/java/com/microservices/spring/dto/ProposalRequestDto.java
@@ -1,4 +1,27 @@
 package com.microservices.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProposalRequestDto {
+
+    private String name;
+
+    private String lastName;
+
+    private String cellphone;
+
+    private String cpf;
+
+    private Double rent;
+
+    private Double requestedAmount;
+
+    private int datePaymentLimit;
 }
--- src/main/java/com/microservices/spring/dto/ProposalResponseDto.java
+++ src/main/java/com/microservices/spring/dto/ProposalResponseDto.java
@@ -1,4 +1,33 @@
 package com.microservices.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProposalResponseDto {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String cellphone;
+
+    private String cpf;
+
+    private Double rent;
+
+    private Double requestedAmount;
+
+    private int datePaymentLimit;
+
+    private Boolean approved;
+
+    private String observation;
 }
