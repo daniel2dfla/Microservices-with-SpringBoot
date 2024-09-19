e24f0d3bf8be84095bf0436f254efea00301dac9
daniel2dfla <daniel_2dfla@hotmail.com>
2024-09-19 20:31:57 -0300

    Feature:completed second microservice and configured communication with AWS SNS service

--- proposta-app/src/main/java/com/microservices/spring/PropostaAppApplication.java
+++ proposta-app/src/main/java/com/microservices/spring/PropostaAppApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProposalAppApplication {
+public class PropostaAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProposalAppApplication.class, args);
+		SpringApplication.run(PropostaAppApplication.class, args);
 	}
 
 }
